18abd88b67bee55eb93b8d1852537dab4ad9fe38
grapedge <grapedge@gmail.com>
2020-01-29 18:34:11 +0800

    Updates

---
+++ precache-manifest.9f0e4ed68cd4d3b83da8dd5b0b140437.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d50ba592e228bc8d68525d0eb7cc133",
+    "url": "/react-minesweeper/index.html"
+  },
+  {
+    "revision": "915576846b8ee7917c02",
+    "url": "/react-minesweeper/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "dcb0c90bdd8241c991e9",
+    "url": "/react-minesweeper/static/css/main.d5ce290c.chunk.css"
+  },
+  {
+    "revision": "915576846b8ee7917c02",
+    "url": "/react-minesweeper/static/js/2.2d7c2a9a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-minesweeper/static/js/2.2d7c2a9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcb0c90bdd8241c991e9",
+    "url": "/react-minesweeper/static/js/main.b226ecec.chunk.js"
+  },
+  {
+    "revision": "1c94b94cc235a9c6761a",
+    "url": "/react-minesweeper/static/js/runtime-main.af53a7f3.js"
+  },
+  {
+    "revision": "6ffecb69767f45a8eade19787417f174",
+    "url": "/react-minesweeper/static/media/996.6ffecb69.png"
+  },
+  {
+    "revision": "a7aff6439bea534bd45f8a94b425fa03",
+    "url": "/react-minesweeper/static/media/background.a7aff643.jpg"
+  },
+  {
+    "revision": "14e34939233413d5c006f9f6a687d53b",
+    "url": "/react-minesweeper/static/media/flag.14e34939.png"
+  }
+]);
